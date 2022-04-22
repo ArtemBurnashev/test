0241e2f028c3f52894c90f3d4afc651c12baa926
@abdumovi <abdumovi@gmail.com>
2022-04-22 10:37:06 +0500

    catalog button change

--- components/carousel/carusel-style.ts
+++ components/carousel/carusel-style.ts
@@ -1,9 +1,19 @@
 import styled from "styled-components";
 
+
 const CategoryCaruselCard = styled.div`
   max-width: 100%;
   overflow: hidden;
   margin: 2rem 0;
+  @media (max-width: 768px){
+    margin: 1rem 0;
+    h6{
+      font-size: 14px;
+    }
+  }
+  .slick-track{
+    left: 0;
+  }
 `;
 
 
