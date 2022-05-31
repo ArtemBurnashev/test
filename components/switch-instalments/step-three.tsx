c9515716a77af5a42ec1104c89fffb3d76d44d0f
Abduraxmon Vahobov <abdumovi@gmail.com>
2022-05-31 17:09:32 +0500

    fix

--- components/switch-instalments/step-three.tsx
+++ components/switch-instalments/step-three.tsx
@@ -1,9 +1,11 @@
-import { Typography} from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import React from 'react';
 import * as Yup from 'yup';
+// @ts-expect-error
+import InputMask from "react-input-mask";
 import Input from 'components/input/input';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
 import ElementsWrap from './elements-wrap';
 
 
@@ -12,34 +14,30 @@ interface Props {
 }
 
 interface LoginInput {
-  famly: string;
-  job: string;
-  work: string;
-  level: string;
-  salary: string;
-
+  phone: string;
+  secondPhone: string;
 }
 
 const validationSchema = Yup.object().shape({
-  famly: Yup.string().max(1000)
-    .required('Family status'),
-  job: Yup.string().max(1000)
-    .required('Job title required.'),
-  work: Yup.string().max(1000)
-    .required('Place of work required.'),
-  level: Yup.string().max(1000)
-    .required('Experience is required'),
-  salary: Yup.string().max(1000)
-    .required('Salary is required'),
+  phone: Yup
+    .string()
+    .min(15)
+    .required('Phone number required.'),
+  secondPhone: Yup
+    .string()
+    .min(15)
+    .required('Phone number required.'),
+
 });
 
 const StepThree: React.FC<Props> = ({ setStepActive }) => {
 
   const formOptions = { resolver: yupResolver(validationSchema) };
-  const { register, handleSubmit, reset, formState } = useForm<LoginInput>(formOptions);
+  const { register, control, handleSubmit, reset, formState } = useForm<LoginInput>(formOptions);
   const { errors } = formState;
   const onSubmit = (item: LoginInput) => {
-    
+    console.log(item);
+
   }
 
   return (
@@ -47,23 +45,23 @@ const StepThree: React.FC<Props> = ({ setStepActive }) => {
       <ElementsWrap>
         <Typography mb='12px' variant='h2'>Телефоны</Typography>
         <Typography mb='24px'>Введите два дополнительных номера телефонов своих родственников или близких</Typography>
-        <Typography mb='12px'>Семейное положение</Typography>
-        <Input
-          fullWidth
-          type='text'
-          {...register('famly')}
-          error={!!errors.famly?.type}
-          helperText={errors.famly?.message}
+        <Typography mb='12px'>Мобилный номер</Typography>
+        <InputMask {...register('phone')}
+          error={!!errors.phone?.type}
+          helperText={errors.phone?.message}
+          mask="+998999999999" maskChar={null}
         />
-        <Typography mb='12px'>Семейное положение</Typography>
+
+        <Typography mt='24px' mb='12px'>Мобилный номер</Typography>
         <Input
           fullWidth
           type='text'
-          {...register('famly')}
-          error={!!errors.famly?.type}
-          helperText={errors.famly?.message}
+          {...register('secondPhone')}
+          error={!!errors.secondPhone?.type}
+          helperText={errors.secondPhone?.message}
         />
       </ElementsWrap>
+      <Button type='submit'>aefaf</Button>
     </form>
 
   )
